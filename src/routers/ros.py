95ac892802945a44c7d1defb42254838b573ea19
emackinnon1 <emackinnon100390@gmail.com>
2025-08-02 20:40:23 -0600

    Add dummy route

--- src/routers/ros.py
+++ src/routers/ros.py
@@ -1,1 +1,13 @@
 # endpoints to report ros status, restart ros2, or kill pids
+# create dependency to double check ROS status before running commands
+from fastapi import APIRouter, Depends, HTTPException
+
+ros_router = APIRouter(
+    prefix="/ros",
+    responses={404: {"description": "Not found"}},
+    tags=["ros"]
+)
+
+@ros_router.post("/status")
+async def ros_status():
+    return {"status": "running"}
