75cc3476421e56c660c107edfa5a798cf1b45737
emackinnon1 <emackinnon100390@gmail.com>
2025-08-02 20:40:40 -0600

    add dummy route to main

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from src.routers.scripts import scripts_router
 from src.routers.arm import arm_router
+from src.routers.ros import ros_router
 
 app = FastAPI()
 
 app.include_router(scripts_router)
 app.include_router(arm_router)
+app.include_router(ros_router)
 
 @app.get("/")
 async def root():
