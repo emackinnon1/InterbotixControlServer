31064deca72951f41246ffc244cbb0e85adf36b3
emackinnon1 <emackinnon100390@gmail.com>
2025-08-06 15:25:51 -0600

    Add is running endpoint

--- src/routers/ros.py
+++ src/routers/ros.py
@@ -1,6 +1,7 @@
 # endpoints to report ros status, restart ros2, or kill pids
 # create dependency to double check ROS status before running commands
 from fastapi import APIRouter, Depends, HTTPException
+from src.dependencies.ros_status import ROSLaunchManager
 
 ros_router = APIRouter(
     prefix="/ros",
@@ -8,6 +9,7 @@ ros_router = APIRouter(
     tags=["ros"]
 )
 
-@ros_router.post("/status")
-async def ros_status():
-    return {"status": "running"}
+@ros_router.post("/running")
+async def ros_running():
+    manager = ROSLaunchManager("wx250")
+    return {"is_running": manager.check_ros2_running()}
