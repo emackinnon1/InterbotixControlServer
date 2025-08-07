b424ecddabb930c52528168ff497aef0af2d4903
emackinnon1 <emackinnon100390@gmail.com>
2025-08-07 12:05:46 -0600

    scripts to tasks

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-from routers.task import scripts_router
+from routers.task import tasks_router
 from src.routers.arm import arm_router
 from src.routers.ros import ros_router
 
 app = FastAPI()
 
-app.include_router(scripts_router)
+app.include_router(tasks_router)
 app.include_router(arm_router)
 app.include_router(ros_router)
 
--- src/routers/task.py
+++ src/routers/task.py
@@ -3,7 +3,7 @@ from enum import Enum
 from fastapi import APIRouter, Depends, HTTPException
 from src.interbotix_ros_manipulators.interbotix_ros_xsarms.interbotix_xsarm_control.scripts.open_beer import open_beer
 # src/interbotix_ros_manipulators/interbotix_ros_xsarms/interbotix_xsarm_control/scripts/open_beer.py
-scripts_router = APIRouter(
+tasks_router = APIRouter(
     prefix="/scripts",
     responses={404: {"description": "Script not found"}},
     tags=["scripts"]
@@ -17,7 +17,7 @@ class BeerBrandEnum(Enum):
     heineken="heineken"
 
 # POST /task/open_beer_bottle?beer_brand=sapporo
-@scripts_router.post("/task/{task_name}")
+@tasks_router.post("/task/{task_name}")
 def perform_task(
     task_name: TasksEnum,
     beer_brand: BeerBrandEnum
