f162ffdb8032360d36af77ebc697364f65d5a322
Elliot Mackinnon <emackinnon100390@gmail.com>
2025-08-24 18:18:59 -0600

    clean up

--- src/routers/task.py
+++ src/routers/task.py
@@ -36,32 +36,3 @@ async def perform_task(
     
     task_func(beer_brand.value)
     return {"task_name": task_name}
-
-# from enum import Enum
-
-# from fastapi import APIRouter, Depends, HTTPException
-# from src.interbotix_ros_manipulators.interbotix_ros_xsarms.interbotix_xsarm_control.scripts.open_beer import open_beer
-# from src.interbotix_ros_manipulators.interbotix_ros_xsarms.interbotix_xsarm_control.scripts.open_beer_state_machine import open_beer_state_machine
-
-# tasks_router = APIRouter(
-#     prefix="/scripts",
-#     responses={404: {"description": "Script not found"}},
-#     tags=["scripts"]
-# )
-
-# class TasksEnum(Enum):
-#     open_beer_bottle="open_beer_bottle"
-#     open_beer_state_machine="open_beer_state_machine"
-
-# class BeerBrandEnum(Enum):
-#     sapporo="sapporo"
-#     heineken="heineken"
-
-# # POST /task/open_beer_bottle?beer_brand=sapporo
-# @tasks_router.post("/task/{task_name}")
-# def perform_task(
-#     task_name: TasksEnum,
-#     beer_brand: BeerBrandEnum
-# ):
-#     {"open_beer_bottle": open_beer}[task_name.value](beer_brand.value)
-#     return {"task_name": task_name}
