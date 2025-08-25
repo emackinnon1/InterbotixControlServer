377605d5a3a174885f62b8c87c6a1f64ac6e2a03
Elliot Mackinnon <emackinnon100390@gmail.com>
2025-08-25 08:38:38 -0600

    Re-add pkgs

---
+++ workspace/build/interbotix_common_toolbox/colcon_command_prefix_build.sh
@@ -0,0 +1,5 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/emackinnon1/InterbotixControlServer/workspace/install/interbotix_common_modules/share/interbotix_common_modules/package.sh"
+. "/home/emackinnon1/InterbotixControlServer/workspace/install/interbotix_common_sim/share/interbotix_common_sim/package.sh"
+. "/home/emackinnon1/InterbotixControlServer/workspace/install/interbotix_xs_msgs/share/interbotix_xs_msgs/package.sh"
+. "/home/emackinnon1/InterbotixControlServer/workspace/install/interbotix_gravity_compensation/share/interbotix_gravity_compensation/package.sh"
