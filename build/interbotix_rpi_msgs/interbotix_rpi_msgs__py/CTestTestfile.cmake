2f664504dba010914c04aa28ccd59ef8826d18a5
Elliot Mackinnon <emackinnon100390@gmail.com>
2025-08-25 09:55:17 -0600

    Remove build

--- build/interbotix_rpi_msgs/interbotix_rpi_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emackinnon1/interbotix_ws/build/interbotix_rpi_msgs/interbotix_rpi_msgs__py
-# Build directory: /home/emackinnon1/interbotix_ws/build/interbotix_rpi_msgs/interbotix_rpi_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
