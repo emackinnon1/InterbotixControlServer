aab8cffaa25206c482b3a16e3a21668fec510e78
Elliot Mackinnon <emackinnon100390@gmail.com>
2026-01-10 16:07:14 -0700

    Add lifespan event for ros startup shutdown

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import logging
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
 from src.routers.task import tasks_router
 from src.routers.arm import arm_router
 from src.routers.ros import ros_router
 from src.routers.state_machine import state_machine_router
+
+from src.dependencies.ros_manager import get_ros_manager
+
+
 
 # Configure logging to show INFO level and above
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    ros_manager = get_ros_manager()
+    await ros_manager.start_ros_launch()
+    yield
+    await ros_manager.stop_ros_launch()
+
 
 app = FastAPI()
 
